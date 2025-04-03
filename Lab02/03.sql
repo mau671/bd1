d00a00c8288b3b2ad6032f74cb9c2d965c30940c
Mauricio González Prendas <me@maugp.com>
2025-04-03 17:15:46 -0600

    Add SQL script to insert phone types and associated phone numbers into GE.TYPE_PHONE and GE.PHONE

---
+++ Lab02/03.sql
@@ -0,0 +1,49 @@
+-- Archivo: 3_insert_telefonos.sql
+-- Descripción:
+--   Inserta registros en las tablas GE.TYPE_PHONE y GE.PHONE.
+--   Se registran al menos los tipos 'Casa', 'Celular' y 'Oficina', permitiendo
+--   agregar más tipos en el futuro. Luego se insertan 8 teléfonos asociados a dichos tipos.
+
+-- Primero, inserción de tipos de teléfono en GE.TYPE_PHONE.
+-- Se sugiere que los IDs sean asignGEos manualmente o mediante secuencia.
+INSERT INTO GE.TYPE_PHONE (id, name)
+VALUES (1, 'Casa');
+
+INSERT INTO GE.TYPE_PHONE (id, name)
+VALUES (2, 'Celular');
+
+INSERT INTO GE.TYPE_PHONE (id, name)
+VALUES (3, 'Oficina');
+
+-- Se pueden agregar más tipos si se requiere, por ejemplo:
+INSERT INTO GE.TYPE_PHONE (id, name)
+VALUES (4, 'Fax');
+
+-- Inserción de 8 teléfonos en GE.PHONE.
+-- Se asume que el campo 'number' contiene el número de teléfono,
+-- y 'id_type_phone' referencia el tipo registrGEo.
+INSERT INTO GE.PHONE (id, number, id_type_phone)
+VALUES (1, '555-1234', 1);
+
+INSERT INTO GE.PHONE (id, number, id_type_phone)
+VALUES (2, '555-5678', 2);
+
+INSERT INTO GE.PHONE (id, number, id_type_phone)
+VALUES (3, '555-9012', 3);
+
+INSERT INTO GE.PHONE (id, number, id_type_phone)
+VALUES (4, '555-3456', 1);
+
+INSERT INTO GE.PHONE (id, number, id_type_phone)
+VALUES (5, '555-7890', 2);
+
+INSERT INTO GE.PHONE (id, number, id_type_phone)
+VALUES (6, '555-2345', 3);
+
+INSERT INTO GE.PHONE (id, number, id_type_phone)
+VALUES (7, '555-6789', 4);
+
+INSERT INTO GE.PHONE (id, number, id_type_phone)
+VALUES (8, '555-0123', 1);
+
+COMMIT;
