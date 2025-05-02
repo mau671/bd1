e15685e4cf3178280b1d8a8facd87386fef4948f
Mauricio González Prendas <me@maugp.com>
2025-05-01 20:20:59 -0600

    Update job_action and repeat_interval in scheduler job to insert new employee hourly

--- Lab07/03.sql
+++ Lab07/03.sql
@@ -2,12 +2,12 @@ BEGIN
   DBMS_SCHEDULER.create_job (
     job_name        => 'A01',
     job_type        => 'PLSQL_BLOCK',
-    job_action      => 'BEGIN utils.newEmployee; END;',
+    job_action      => 'BEGIN GE.employee_package.newEmployee(1, ''John'', ''A'', ''Doe'', ''Smith'', 5000, SYSDATE); END;',
     start_date      => SYSTIMESTAMP,
-    repeat_interval => 'freq=secondly',
+    repeat_interval => 'freq=hourly',
     end_date        => NULL,
     enabled         => TRUE,
-    comments        => 'Mi primer job'
+    comments        => 'Insert new employee every hour'
   );
 END;
 /
