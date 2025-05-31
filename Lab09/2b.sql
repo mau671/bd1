90fef0d39c69271adfeffc8a0bc976176edfeb6f
Mauricio González Prendas <me@maugp.com>
2025-05-31 10:05:08 -0600

    Update comment to clarify access permissions for employee table

--- Lab09/2b.sql
+++ Lab09/2b.sql
@@ -6,7 +6,7 @@ GRANT SELECT ON salary_per_employee TO cu;
 SELECT * FROM ge.salary_per_employee;
 
 -- Puede CU acceder a los datos que están en la view (nombre, ID y salario) directamente en employee? 
--- 
+-- No, porque no tiene permiso de SELECT en la tabla employee.
 
 -- b) Intentar leer directamente de la tabla GE.EMPLOYEE: falla
 SELECT id, first_name, salary FROM ge.employee;
